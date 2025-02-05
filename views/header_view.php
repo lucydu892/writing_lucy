32a5b4027fbd6b00d7fce29597905ce450d29eea
Lucy Duss <lucyduss@outlook.com>
2025-02-05 15:57:10 +0100

    verbesserung vom design

--- views/header_view.php
+++ views/header_view.php
@@ -1,5 +1,5 @@
 <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand" href="home">Writing Lucy</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
