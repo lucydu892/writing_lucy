0e5bb357087557995bdec16bd59a3add5f841bb0
Lucy Duss <lucyduss@outlook.com>
2025-04-10 17:02:32 +0200

    erste testversion von dropdown texteditor

--- views/home_view.php
+++ views/home_view.php
@@ -17,59 +17,45 @@ require "models/home_model.php";
                     Textbearbeitung
                 </p>
                 <div class="dropdownTextEditor" id="dropdownTextEditor">
-                    <p>1</p>
+                    <label for="bgColor">Hintergrundfarbe:</label>
                     <br>
-                    <p>2</p>
+                    <input id="bgColor" type="color" name="bgColor">
                     <br>
-                    <p>3</p>
+                    <br>
+                    <label for="fontColor">Schriftfarbe:</label>
+                    <br>
+                    <input id="fontColor" type="color" name="fontColor">
+                    <br>
+                    <label for="fontSize">Schriftgrösse:</label>
+                    <br>
+                    <input id="fontSize" type="number" maxlength="3" name="fontSize">
+                    <br>
+                    <label for="fontFamily">Schriftart:</label>
+                    <br>
+                    <select name="fontFamily" id="fontFamily">
+                        <option value="">Bitte wählen</option>
+                        <option value="Arial">Arial</option>
+                        <option value="Palatino Linotype">Palatino Linotype</option>
+                        <option value="Helvetica">Helvetica</option>
+                        <option value="Impact">Impact</option>
+                        <option value="Times New Roman serif">Times New Roman serif</option>
+                        <option value="Georgia">Georgia serif</option>
+                        <option value="Lucida Sans Unicode">Lucida Sans Unicode</option>
+                        <option value="Courier New monospace">Courier New monospace</option>
+                        <option value="Arial Black">Arial Black</option>
+                    </select>
+                    <br>
+                    <label for="fontDeco">Textdekoration:</label>
+                    <br>
+                    <select name="fontDeco" id="fontDeco">
+                        <option value="1">Bitte wählen</option>
+                        <option value="None">None</option>
+                        <option value="line-through">line-through</option>
+                        <option value="underline">underline</option>
+                    </select>
+                    <br>
                 </div>
             </div>
-            <div>
-                <label for="bgColor">Hintergrundfarbe:</label>
-                <br>
-                <input id="bgColor" type="color" name="bgColor">
-            </div>
-            <br>
-            <div>
-                <label for="fontColor">Schriftfarbe:</label>
-                <br>
-                <input id="fontColor" type="color" name="fontColor">
-            </div>
-            <br>
-            <div>
-                <label for="fontSize">Schriftgrösse:</label>
-                <br>
-                <input id="fontSize" type="number" maxlength="3" name="fontSize">
-            </div>
-            <br>
-            <div>
-                <label for="fontFamily">Schriftart:</label>
-                <br>
-                <select name="fontFamily" id="fontFamily">
-                    <option value="">Bitte wählen</option>
-                    <option value="Arial">Arial</option>
-                    <option value="Palatino Linotype">Palatino Linotype</option>
-                    <option value="Helvetica">Helvetica</option>
-                    <option value="Impact">Impact</option>
-                    <option value="Times New Roman serif">Times New Roman serif</option>
-                    <option value="Georgia">Georgia serif</option>
-                    <option value="Lucida Sans Unicode">Lucida Sans Unicode</option>
-                    <option value="Courier New monospace">Courier New monospace</option>
-                    <option value="Arial Black">Arial Black</option>
-                </select>
-            </div>
-            <br>
-            <div>
-                <label for="fontDeco">Textdekoration:</label>
-                <br>
-                <select name="fontDeco" id="fontDeco">
-                    <option value="1">Bitte wählen</option>
-                    <option value="None">None</option>
-                    <option value="line-through">line-through</option>
-                    <option value="underline">underline</option>
-                </select>
-            </div>
-            <br>
             <label for="text">Dein Text:</label>
             <textarea name="text" id="text" cols="30" rows="6"></textarea>
             <br>
