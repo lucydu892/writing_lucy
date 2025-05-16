7b2750f10cf7df7255359eec38b5251169af1300
Lucy Duss <lucyduss@outlook.com>
2025-05-16 07:55:44 +0200

    deleted unused test

--- tests/test-1.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost/writing_lucy/register');
-  await page.getByRole('heading', { name: 'Registrieren' }).click();
-  await page.getByRole('textbox', { name: 'E-Mail' }).click();
-  await page.getByRole('textbox', { name: 'E-Mail' }).fill('testUser');
-  await page.getByRole('button', { name: 'Registrieren' }).click();
-});
